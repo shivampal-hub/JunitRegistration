4a02ee5e8c72a82b4dce4d0b31126ab45b2dd52e
shipal <shivamm.ppal@gmail.com>
2022-05-11 13:53:23 +0530

    Update : UC4-MobileNumber

---
+++ Junit4/src/com/bl/regex/Constants.java
@@ -0,0 +1,14 @@
+package com.bl.regex;
+
+public class Constants {
+
+    private Constants() {
+
+    }
+
+    public final static String FirstName_REGEX = "^[A-Z][a-z]{2,}$";
+    public final static String LastName_REGEX = "^[A-Z][a-z]{2,}$";
+    public final static String EMAIL_REGEX = "^[a-z0-9]{1,}([.+-][0-9a-z]{1,})?@[a-z0-9]{1,}+(.[a-z]{2,4})([.+-][a-z]{1,3})?$";
+    public final static String MOBILE_REGEX = "^[0-9]{2}[ ][6-9]{1}[0-9]{9}$";
+
+}
